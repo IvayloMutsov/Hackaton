9ada9e0d56055e305674d20a536a7d6c3e6d86db
Martin Marinov <m.marinov1733@gmail.com>
2025-10-24 10:27:55 +0300

    корекция

--- Hackaton/Models/Procedures.cs
+++ Hackaton/Models/Procedures.cs
@@ -7,12 +7,14 @@ namespace Hackaton.Models
         public int Id { get; set; } 
         public DateTime? Date { get; set; }
         public string ProcedureType { get; set; }
-        public int ProfessorId1 { get; set; }
-        public int ProfessorId2 { get; set; }
-        public int ProfessorId3 { get; set; }
-        public int ProfessorId4 { get; set; }
-        public int ProfessorId5 { get; set; }
-        public int ProfessorId6 { get; set; }
-        public int ProfessorId7 { get; set; }
+        public int? ProfessorId1 { get; set; }
+        public int? ProfessorId2 { get; set; }
+        public int? ProfessorId3 { get; set; }
+        public int? ProfessorId4 { get; set; }
+        public int? ProfessorId5 { get; set; }
+        public int? ProfessorId6 { get; set; }
+        public int? ProfessorId7 { get; set; }
+        public int? ProfessorId7 { get; set; }
+        public int? ProfessorId7 { get; set; }
     }
 }

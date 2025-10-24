e115b3272ab230fecca61cce7674f50ef2279095
Martin Marinov <m.marinov1733@gmail.com>
2025-10-24 13:41:03 +0300

    таблица

--- Hackaton/Controllers/HomeController.cs
+++ Hackaton/Controllers/HomeController.cs
@@ -1,21 +1,26 @@
 using System.Diagnostics;
+using Hackaton.Data;
 using Hackaton.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Hackaton.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context; 
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var procedures = await _context.Procedures.ToListAsync();
+            return View(procedures);
         }
 
         public IActionResult Privacy()

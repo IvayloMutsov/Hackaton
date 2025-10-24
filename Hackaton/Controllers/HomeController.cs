cc82c8d55cde442319aad8dc7ab50fc56272b9a3
Martin Marinov <m.marinov1733@gmail.com>
2025-10-24 14:13:45 +0300

    бутон

--- Hackaton/Controllers/HomeController.cs
+++ Hackaton/Controllers/HomeController.cs
@@ -23,11 +23,6 @@ namespace Hackaton.Controllers
             return View(procedures);
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {

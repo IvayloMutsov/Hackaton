509e7a437aaa73a112e7c6e0217e1da79c7fa5e9
Martin Marinov <m.marinov1733@gmail.com>
2025-10-24 13:19:06 +0300

    file

--- Hackaton/Data/Reader/DbInitializer.cs
+++ Hackaton/Data/Reader/DbInitializer.cs
@@ -13,54 +13,63 @@ namespace Hackaton.Data.Reader
 
             string filePath = "Database.xlsx";
 
-            //using (var package = new ExcelPackage(new FileInfo(filePath)))
-            //{
-            //    var worksheet = package.Workbook.Worksheets[0];
-            //    int rowCount = worksheet.Dimension.Rows;
-            //    int colCount = worksheet.Dimension.Columns;
+            using (var package = new ExcelPackage(new FileInfo(filePath)))
+            {
+                var worksheet = package.Workbook.Worksheets[0];
+                int rowCount = worksheet.Dimension.Rows;
+                int colCount = worksheet.Dimension.Columns;
 
-            //    // Start from row 2 to skip header
-            //    for (int row = 2; row <= rowCount; row++)
-            //    {
-            //        Proffessors prof = new Proffessors();
-            //        var cells = new List<string>();
-            //        for (int col = 2; col <= colCount; col++)
-            //        {
-            //            var cellValue = worksheet.Cells[row, col].Text;
-            //            switch (col)
-            //            {
-            //                case 2: prof.AcademicRank = cellValue;
-            //                    break;
-            //                case 3: prof.FullName = cellValue;
-            //                    break;
-            //                case 4: prof.ScientificFiled = cellValue;
-            //                    break;
-            //                case 5: prof.University = cellValue;
-            //                    break;
-            //                case 6: prof.Distance = int.Parse(cellValue);
-            //                    if (prof.Distance == 0)
-            //                    {
-            //                        prof.UniIsLocal = true;
-            //                    }
-            //                    else
-            //                    {
-            //                        prof.UniIsLocal = false;
-            //                    }
-            //                    break;
-            //                case 7: prof.ParticipationCounter = int.Parse(cellValue);
-            //                    break;
-            //                case 8: prof.PrevParticipationDate = DateTime.Parse(cellValue);
-            //                    break;
-            //                case 9: prof.LastParticipationDate = DateTime.Parse(cellValue);
-            //                    break;
-            //                default: throw new Exception("Data is in invalid format!");
-            //                    break;
-            //            }
-            //        }
-            //        context.Proffessors.Add(prof);
-            //    }
-            //    context.SaveChanges();
-            //}
+                // Start from row 2 to skip header
+                for (int row = 2; row <= rowCount; row++)
+                {
+                    Proffessors prof = new Proffessors();
+                    var cells = new List<string>();
+                    for (int col = 2; col <= colCount; col++)
+                    {
+                        var cellValue = worksheet.Cells[row, col].Text;
+                        switch (col)
+                        {
+                            case 2:
+                                prof.AcademicRank = cellValue;
+                                break;
+                            case 3:
+                                prof.FullName = cellValue;
+                                break;
+                            case 4:
+                                prof.ScientificFiled = cellValue;
+                                break;
+                            case 5:
+                                prof.University = cellValue;
+                                break;
+                            case 6:
+                                prof.Distance = int.Parse(cellValue);
+                                if (prof.Distance == 0)
+                                {
+                                    prof.UniIsLocal = true;
+                                }
+                                else
+                                {
+                                    prof.UniIsLocal = false;
+                                }
+                                break;
+                            case 7:
+                                prof.ConsecutiveCounter = int.Parse(cellValue);
+                                break;
+                            case 8:
+                                prof.PrevParticipationDate = DateTime.Parse(cellValue);
+                                break;
+                            case 9:
+                                prof.LastParticipationDate = DateTime.Parse(cellValue);
+                                break;
+                            default:
+                                throw new Exception("Data is in invalid format!");
+                                break;
+                        }
+                    }
+                    context.Proffessors.Add(prof);
+                }
+                context.SaveChanges();
+            }
         }
     }
 }
